9bfe108d7fd82dac77f62d8f66550dc760e230fd
danilDz <freekick2017uk@gmail.com>
2022-02-08 17:27:39 +0200

    Новый проект и работа с сервером + трансформация данных и компонент со случайным персонажем

---
+++ README.md
@@ -0,0 +1,5 @@
+# Marvel-Project
+
+This website is created for people, who loves cinematic universe Marvel.
+
+This functional is created by using Marvel API.
---
+++ src/services/MarvelService.js
@@ -0,0 +1,46 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=bd45ac9cd4c42c0d44799afb637c8179';
+
+    getResources = async (url) => {
+        let data = await fetch(url);
+
+        if (!data.ok) {
+            throw new Error(`Couldn't fetch ${url}, status ${data.status}`);
+        }
+
+        return await data.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResources(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0])
+    }
+
+    _transformCharacter = (char) => {
+        let desc = ''
+        if (char.description) {
+            if (char.description.length < 210) {
+                desc = char.description
+            } else {
+                desc = char.description.slice(0, 210) + '...'
+            }
+        } else {
+            desc = 'Informaion about this character not found!'
+        }
+        return {
+            name: char.name,
+            description: desc,
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
